576af04f707923a034e1495ee649edc82516b04f
Aaron Elkiss <aelkiss@umich.edu>
2018-04-09 17:17:58 -0400

    WIP - add audit status endpoint

--- spec/controllers/v1/audit_controller_spec.rb
+++ spec/controllers/v1/audit_controller_spec.rb
@@ -3,12 +3,41 @@
 require "rails_helper"
 
 RSpec.describe V1::AuditController, type: :controller do
+
+  describe "/v1" do
+    describe "GET #index" do
+      context "as an admin" do
+        include_context "as admin user" do
+          it "assigns an audit presenter"
+        end
+      end
+
+      context "as a non-admin" do
+        include_context "as underprivileged user"
+
+        it "returns a 403" do
+          request.headers.merge! auth_header
+          get :index
+          expect(response).to have_http_status(403)
+        end
+          
+      end
+
+      context "as a user that is not logged in" do
+        it "returns a 401" do
+          get :index
+          expect(response).to have_http_status(401)
+        end
+      end
+    end
+  end
+
   describe "/v1" do
     describe "POST #create" do
       # create two packages
       context "as an administrator" do
         include_context "as admin user"
-        it "starts a FixityCheckJob for each package" do
+        it "starts a FixityCheckJob for each stored package" do
           request.headers.merge! auth_header
           allow(FixityCheckJob).to receive(:perform_later)
           packages = Array.new(2) { Fabricate(:package) }
@@ -18,6 +47,9 @@ RSpec.describe V1::AuditController, type: :controller do
           packages.each do |package|
             expect(FixityCheckJob).to have_received(:perform_later).with(package, user)
           end
+        end
+
+        it "does not start a FixityCheckJob for unstored packages (requests)" do
         end
       end
 
--- spec/routing/v1/queue_item_routing_spec.rb
+++ spec/routing/v1/queue_item_routing_spec.rb
@@ -15,9 +15,5 @@ RSpec.describe V1::QueueItemsController, type: :routing do
     it "routes to #show" do
       expect(get: "/v1/queue/1").to route_to("v1/queue_items#show", id: "1")
     end
-
-    xit "routes to #destroy" do
-      expect(delete: "/v1/queue/1").to route_to("v1/queue_items#destroy", id: "1")
-    end
   end
 end
