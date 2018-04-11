c43313c11fa25858478806d0b0d9a75d5d6249e9
Aaron Elkiss <aelkiss@umich.edu>
2018-04-11 08:10:18 -0400

    Address feedback from #40

--- app/controllers/v1/events_controller.rb
+++ app/controllers/v1/events_controller.rb
@@ -1,11 +1,7 @@
 module V1
   class EventsController < ApplicationController
     def index
-      events = Event
-      events = events.bag(params[:bag_id]) if params[:bag_id]
-
-      @events = policy_scope(events)
-
+      @events = policy_scope(Event.bag(params[:bag_id]))
     end
   end
 end
