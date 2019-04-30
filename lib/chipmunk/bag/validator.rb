0bede81e83c17db075d2ed87efd3bd5b69827504
Noah Botimer <botimer@umich.edu>
2019-04-30 18:10:40 -0400

    Fix indentation on validation files

--- lib/chipmunk/bag/validator.rb
+++ lib/chipmunk/bag/validator.rb
@@ -1,82 +1,82 @@
 # frozen_string_literal: true
 
 class Chipmunk::Bag
-class Validator
-  include Chipmunk::Validatable
+  class Validator
+    include Chipmunk::Validatable
 
-  attr_reader :errors
+    attr_reader :errors
 
-  # TODO: Decide exactly what this should take... Package, Bag, or what?
-  # @param package [Package]
-  # @param bag [Chipmunk::Package] If nil, the bag does not yet exist.
-  def initialize(package, errors = [], bag = nil)
-    @package = package
-    @src_path = package.src_path
-    @bag = bag
-    @errors = errors
-  end
-
-  validates "bag must exist on disk at src_path",
-    condition: -> { File.exist?(src_path) },
-    error: -> { "Bag does not exist at upload location #{src_path}" }
-
-  validates "bag on disk must be valid",
-    condition: -> { bag.valid? },
-    error: -> { "Error validating bag:\n" + indent_array(bag.errors.full_messages) }
-
-  { "External-Identifier"   => :external_id,
-    "Bag-ID"                => :bag_id,
-    "Chipmunk-Content-Type" => :content_type }.each_pair do |file_key, db_key|
-      validates "#{file_key} in bag on disk matches bag in database",
-        precondition: -> { [bag.chipmunk_info[file_key], package.public_send(db_key)] },
-        condition:  ->(file_val, db_val) { file_val == db_val },
-        error: lambda {|file_val, db_val|
-          "uploaded #{file_key} '#{file_val}'" \
-                " does not match expected value '#{db_val}'"
-        }
+    # TODO: Decide exactly what this should take... Package, Bag, or what?
+    # @param package [Package]
+    # @param bag [Chipmunk::Package] If nil, the bag does not yet exist.
+    def initialize(package, errors = [], bag = nil)
+      @package = package
+      @src_path = package.src_path
+      @bag = bag
+      @errors = errors
     end
 
-  validates "Bag ID in bag on disk matches bag in database",
-    condition:  -> { bag.chipmunk_info["Bag-ID"] == package.bag_id },
-    error: lambda {
-      "uploaded Bag-ID '#{bag.chipmunk_info["Bag-ID"]}'" \
-          " does not match intended ID '#{package.bag_id}'"
-    }
+    validates "bag must exist on disk at src_path",
+      condition: -> { File.exist?(src_path) },
+      error: -> { "Bag does not exist at upload location #{src_path}" }
 
-  metadata_tags = ["Metadata-URL", "Metadata-Type", "Metadata-Tagfile"]
+    validates "bag on disk must be valid",
+      condition: -> { bag.valid? },
+      error: -> { "Error validating bag:\n" + indent_array(bag.errors.full_messages) }
 
-  validates "chipmunk-info.txt has metadata tags",
-    error: -> { "Some (but not all) metadata tags #{metadata_tags} missing in chipmunk-info.txt" },
-    condition: lambda {
-      metadata_tags.all? {|tag| bag.chipmunk_info.key?(tag) } ||
-        metadata_tags.none? {|tag| bag.chipmunk_info.key?(tag) }
-    }
+    { "External-Identifier"   => :external_id,
+      "Bag-ID"                => :bag_id,
+      "Chipmunk-Content-Type" => :content_type }.each_pair do |file_key, db_key|
+        validates "#{file_key} in bag on disk matches bag in database",
+          precondition: -> { [bag.chipmunk_info[file_key], package.public_send(db_key)] },
+          condition:  ->(file_val, db_val) { file_val == db_val },
+          error: lambda {|file_val, db_val|
+            "uploaded #{file_key} '#{file_val}'" \
+                  " does not match expected value '#{db_val}'"
+          }
+      end
 
-  validates "bag on disk has referenced metadata files",
-    only_if: -> { bag.chipmunk_info["Metadata-Tagfile"] },
-    error: -> { "Missing referenced metadata #{bag.chipmunk_info["Metadata-Tagfile"]}" },
-    condition: lambda {
-      bag.tag_files.map {|f| File.basename(f) }
-        .include?(bag.chipmunk_info["Metadata-Tagfile"])
-    }
+    validates "Bag ID in bag on disk matches bag in database",
+      condition:  -> { bag.chipmunk_info["Bag-ID"] == package.bag_id },
+      error: lambda {
+        "uploaded Bag-ID '#{bag.chipmunk_info["Bag-ID"]}'" \
+            " does not match intended ID '#{package.bag_id}'"
+      }
 
-  validates "bag on disk passes external validation",
-    only_if: -> { package.external_validation_cmd },
-    precondition: -> { Open3.capture3(package.external_validation_cmd) },
-    condition: ->(_, _, status) { status.exitstatus.zero? },
-    error: ->(_, stderr, _) { "Error validating content\n" + stderr }
+    metadata_tags = ["Metadata-URL", "Metadata-Type", "Metadata-Tagfile"]
 
-  validates "bag on disk meets bagger profile",
-    only_if: -> { package.bagger_profile },
-    condition: -> { BaggerProfile.new(package.bagger_profile).valid?(bag.bag_info, errors: errors) },
-    error: -> { "Not valid according to bagger profile" }
+    validates "chipmunk-info.txt has metadata tags",
+      error: -> { "Some (but not all) metadata tags #{metadata_tags} missing in chipmunk-info.txt" },
+      condition: lambda {
+        metadata_tags.all? {|tag| bag.chipmunk_info.key?(tag) } ||
+          metadata_tags.none? {|tag| bag.chipmunk_info.key?(tag) }
+      }
 
-  private
+    validates "bag on disk has referenced metadata files",
+      only_if: -> { bag.chipmunk_info["Metadata-Tagfile"] },
+      error: -> { "Missing referenced metadata #{bag.chipmunk_info["Metadata-Tagfile"]}" },
+      condition: lambda {
+        bag.tag_files.map {|f| File.basename(f) }
+          .include?(bag.chipmunk_info["Metadata-Tagfile"])
+      }
 
-  def indent_array(array, width = 2)
-    array.map {|s| " " * width + s }.join("\n")
+    validates "bag on disk passes external validation",
+      only_if: -> { package.external_validation_cmd },
+      precondition: -> { Open3.capture3(package.external_validation_cmd) },
+      condition: ->(_, _, status) { status.exitstatus.zero? },
+      error: ->(_, stderr, _) { "Error validating content\n" + stderr }
+
+    validates "bag on disk meets bagger profile",
+      only_if: -> { package.bagger_profile },
+      condition: -> { BaggerProfile.new(package.bagger_profile).valid?(bag.bag_info, errors: errors) },
+      error: -> { "Not valid according to bagger profile" }
+
+    private
+
+    def indent_array(array, width = 2)
+      array.map {|s| " " * width + s }.join("\n")
+    end
+
+    attr_reader :src_path, :package, :bag
   end
-
-  attr_reader :src_path, :package, :bag
-end
 end
