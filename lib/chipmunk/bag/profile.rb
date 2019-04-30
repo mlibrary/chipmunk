01bfb35072faabd3b49ff06dbc1e0a8758e1aadf
Noah Botimer <botimer@umich.edu>
2019-04-30 19:16:07 -0400

    Reindent Profile and Tag

--- lib/chipmunk/bag/profile.rb
+++ lib/chipmunk/bag/profile.rb
@@ -5,25 +5,25 @@ require "open-uri"
 require "uri"
 
 class Chipmunk::Bag
-class Profile
+  class Profile
 
-  def initialize(uri)
-    uri = URI.parse(uri)
-    profile = if uri.scheme == "file"
-      File.read(uri.path)
-    else
-      uri.read
+    def initialize(uri)
+      uri = URI.parse(uri)
+      profile = if uri.scheme == "file"
+        File.read(uri.path)
+      else
+        uri.read
+      end
+
+      @tags = JSON.parse(profile)["ordered"].map {|t| Tag.from_hash(t) }
     end
 
-    @tags = JSON.parse(profile)["ordered"].map {|t| Tag.from_hash(t) }
+    def valid?(bag_info, errors: [])
+      tags.map {|tag| tag.value_valid?(bag_info[tag.name], errors: errors) }.all? {|valid| valid == true }
+    end
+
+    private
+
+    attr_reader :tags
   end
-
-  def valid?(bag_info, errors: [])
-    tags.map {|tag| tag.value_valid?(bag_info[tag.name], errors: errors) }.all? {|valid| valid == true }
-  end
-
-  private
-
-  attr_reader :tags
-end
 end
