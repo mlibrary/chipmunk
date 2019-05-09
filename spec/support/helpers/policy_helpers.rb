6d455be87de24fb4ee4a636de03af05bfbe530d4
Aaron Elkiss <aelkiss@umich.edu>
2019-05-09 13:41:29 -0400

    WIP: custom matchers for policies
    e.g. expect(policy).to resolve(whatever)
    
    The allow_action matcher doesn't seem to work because of issues finding
    Checkpoint::Resource.from; unclear why

--- spec/support/helpers/policy_helpers.rb
+++ spec/support/helpers/policy_helpers.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'rspec/expectations'
+
 class FakeUser < OpenStruct
   def initialize(hash = {})
     user_name = Faker::Internet.user_name
@@ -36,6 +38,33 @@ class FakeCollection < OpenStruct
     [:owned, user_id]
   end
 
+  def with_type(type)
+    [:type, type]
+  end
+
+  def with_id(id)
+    [:id, id]
+  end
+
+  def with_type_and_id_(type, id)
+    [:type_and_id, type, id]
+  end
+
+  def any_of(scopes)
+    scopes
+  end
+end
+
+RSpec::Matchers.define :allow_action do |action|
+  match do |policy|
+    if policy <= CollectionPolicy
+      policy.new(user, FakeCollection.new).public_send(action)
+    elsif policy <= ResourcePolicy
+      policy.new(user, resource).public_send(action)
+    else
+      raise "#{described_class} is not a subclass of CollectionPolicy or ResourcePolicy"
+    end
+  end
 end
 
 def allows_action?(action)
@@ -52,6 +81,7 @@ def it_allows(*actions)
   actions.each do |action|
     it "is allowed to #{action}" do
       expect(allows_action?(action)).to be(true)
+#      expect(described_class).to allow_action(action)
     end
   end
 end
@@ -60,6 +90,22 @@ def it_disallows(*actions)
   actions.each do |action|
     it "is not allowed to #{action}" do
       expect(allows_action?(action)).to be(false)
+#      expect(described_class).not_to allow_action(action)
+    end
+  end
+end
+
+RSpec::Matchers.define :resolve do |expected|
+  match do |policy|
+    actual = policy.new(user, FakeCollection.new).resolve
+
+    case expected
+    when :all
+      actual == :all || actual == [:all]
+    when :none
+      actual == :none || actual == []
+    else
+      actual == expected
     end
   end
 end
@@ -67,7 +113,7 @@ end
 def it_resolves(scope)
   describe "#resolve" do
     it "resolves to #{scope}" do
-      expect(described_class.new(user, FakeCollection.new).resolve).to eq(scope)
+      expect(described_class).to resolve(scope)
     end
   end
 end
@@ -75,15 +121,23 @@ end
 def it_resolves_owned
   describe "#resolve" do
     it "resolves using the owned scope with the current user" do
-      expect(described_class.new(user, FakeCollection.new).resolve).to eq([:owned, user.id])
+      expect(described_class).to resolve([:owned, user.id])
     end
+  end
+end
+
+RSpec::Matchers.define :have_base_scope do |scope|
+  match do |policy|
+    policy.new(double).base_scope == scope
   end
 end
 
 def it_has_base_scope(scope)
   describe "#base_scope" do
     it "returns all events" do
-      expect(described_class.new(double).base_scope).to eq(scope)
+      expect(described_class).to have_base_scope(scope)
     end
   end
 end
+
+
