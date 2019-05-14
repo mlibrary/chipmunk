29d4ab2d8112e54da97dd928bc66fc1a9305f864
Aaron Elkiss <aelkiss@umich.edu>
2019-05-14 12:08:27 -0400

    PFDR-169: move QueueItemBuilder into QueueItemsController
    There is a pretty clear split of responsibility between the public
    controller methods and the private methods that are largely moved
    from QueueItemBuilder, but it isn't really clear how to cleanly
    re-extract it.
    
    In particular, this would either need to:
    
    - do a lot of passing of the queue item back & forth for
    authorization (similar to the previous implementation)
    
    - know about the current user and the policy classes, such that the
    QueueItemBuilder could instantiate the policy at the appropriate time
    
    - be provided 'partially bound' policies (i.e. policies bound to the
    current user but not the resource)

--- app/controllers/v1/queue_items_controller.rb
+++ app/controllers/v1/queue_items_controller.rb
@@ -1,11 +1,13 @@
 # frozen_string_literal: true
 
-require "queue_item_builder"
+# require "queue_item_builder"
 
 module V1
   class QueueItemsController < ResourceController
     collection_policy QueueItemsPolicy
     resource_policy QueueItemPolicy
+
+    attr_writer :bag_move_job
 
     # GET /v1/queue
     def index
@@ -24,24 +26,48 @@ module V1
 
     # POST /v1/requests/:bag_id/complete
     def create
-
-      # FIXME PFDR-169 needs some cleanup - this should probably do something different
       descriptor = Package.find_by_bag_id!(params[:bag_id])
-      builder = QueueItemBuilder.new
-      @queue_item = builder.build(descriptor)
-      policy = resource_policy.new(current_user, @queue_item)
-      policy.authorize! :create?, "not allowed to create? this QueueItem for #{params[:bag_id]}"
-      status, @queue_item = builder.save
 
-      case status
-      when :duplicate
-        head 303, location: v1_queue_item_path(@queue_item)
-      when :created
-        head 201, location: v1_queue_item_path(@queue_item)
-      when :invalid
-        render json: @queue_item.errors, status: :unprocessable_entity
+      if duplicate = QueueItem.where(package: descriptor, status: [:pending, :done]).first
+        head 303, location: v1_queue_item_path(duplicate)
       else
-        raise "Unexpected status: #{status.inspect} for queue item #{@queue_item.id}"
+        case create_qitem(descriptor)
+        when :created
+          head 201, location: v1_queue_item_path(@queue_item)
+        when :invalid
+          render json: @queue_item.errors, status: :unprocessable_entity
+        else
+          raise "Unexpected status: #{status.inspect} for queue item #{@queue_item.id}"
+        end
+      end
+    end
+
+    private
+
+    def bag_move_job
+      @bag_move_job ||= BagMoveJob
+    end
+
+    def create_qitem(descriptor)
+      @queue_item = new_qitem(descriptor)
+      save_qitem(@queue_item)
+    end
+
+    def new_qitem(descriptor)
+      collection_policy.new(current_user).authorize! :new?
+
+      QueueItem.new(package: descriptor)
+    end
+
+    def save_qitem(queue_item)
+      resource_policy.new(current_user,queue_item).authorize! :create?
+
+      if queue_item.valid?
+        queue_item.save!
+        bag_move_job.perform_later(queue_item)
+        return :created
+      else
+        return :invalid
       end
     end
   end
--- spec/controllers/v1/queue_items_controller_spec.rb
+++ spec/controllers/v1/queue_items_controller_spec.rb
@@ -84,37 +84,45 @@ RSpec.describe V1::QueueItemsController, type: :controller do
       include_context "with someone logged in"
 
       let!(:package) { Fabricate(:package, user: user) }
-      let(:result_queue_item) { Fabricate(:queue_item, package_id: package.bag_id) }
       let(:result_status) { status }
       let(:builder) { double(:builder) }
       let(:status) { nil }
+      let(:bag_move_job) { double(:bag_move_job,perform_later: nil) }
 
       before(:each) do
-        allow(QueueItemBuilder).to receive(:new).and_return(builder)
-        allow(builder).to receive(:build).and_return(result_queue_item)
-        allow(builder).to receive(:save).and_return([result_status, result_queue_item])
-        resource_policy "QueueItemPolicy", create?: true
+        controller.bag_move_job = bag_move_job
       end
 
-      context "QueueItemBuilder returns status==:duplicate" do
-        let(:status) { :duplicate }
-
+      shared_examples "a duplicate" do
+        before(:each) do
+          resource_policy "QueueItemPolicy", show?: true
+        end
         it "responds with 303" do
           post :create, params: { bag_id: package.bag_id }
           expect(response).to have_http_status(303)
         end
         it "populates the location header" do
           post :create, params: { bag_id: package.bag_id }
-          expect(response.location).to eql(v1_queue_item_path(result_queue_item.id))
+          expect(response.location).to eql(v1_queue_item_path(queue_item.id))
         end
         it "renders nothing" do
           post :create, params: { bag_id: package.bag_id }
           expect(response).to render_template(nil)
         end
+        it "does not create a queue item" do
+          expect { post :create, params: { bag_id: package.bag_id } }.not_to change { QueueItem.count }
+        end
+        it "does not enqueue a BagMoveJob" do
+          expect(bag_move_job).not_to receive(:perform_later)
+          post :create, params: { bag_id: package.bag_id }
+        end
       end
 
-      context "QueueItemBuilder returns status==:created" do
-        let(:status) { :created }
+      shared_examples "creates a new queue item" do
+        before(:each) do
+          resource_policy "QueueItemPolicy", create?: true
+          collection_policy "QueueItemsPolicy", QueueItem.all, new?: true
+        end
 
         it "responds with 201" do
           post :create, params: { bag_id: package.bag_id }
@@ -122,25 +130,49 @@ RSpec.describe V1::QueueItemsController, type: :controller do
         end
         it "populates the location header" do
           post :create, params: { bag_id: package.bag_id }
-          expect(response.location).to eql(v1_queue_item_path(result_queue_item.id))
+          expect(response.location).to eql(v1_queue_item_path(assigns(:queue_item).id))
         end
         it "renders nothing" do
           post :create, params: { bag_id: package.bag_id }
           expect(response).to render_template(nil)
+        end
+        it "creates a queue item" do
+          expect { post :create, params: { bag_id: package.bag_id } }.to change { QueueItem.count }.by(1)
+        end
+        it "the queue item is for the expected descriptor" do
+          post :create, params: { bag_id: package.bag_id }
+          expect(assigns(:queue_item).package).to eql(package)
+        end
+        it "the queue_item is pending" do
+          post :create, params: { bag_id: package.bag_id }
+          expect(assigns(:queue_item).pending?).to be true
+        end
+        it "enqueues a BagMoveJob" do
+          expect(bag_move_job).to receive(:perform_later)
+          post :create, params: { bag_id: package.bag_id }
         end
       end
 
-      context "QueueItemBuilder returns status==:invalid" do
-        let(:status) { :invalid }
+      context "when a pending queue item already exists" do
+        let!(:queue_item) { Fabricate(:queue_item, package: package, status: :pending) }
 
-        it "responds with 422" do
-          post :create, params: { bag_id: package.bag_id }
-          expect(response).to have_http_status(422)
-        end
-        it "renders nothing" do
-          post :create, params: { bag_id: package.bag_id }
-          expect(response).to render_template(nil)
-        end
+        it_behaves_like "a duplicate"
+      end
+
+      context "when a done queue item already exists" do
+        let!(:queue_item) { Fabricate(:queue_item, package: package, status: :done) }
+
+        it_behaves_like "a duplicate"
+      end
+
+      context "when a failed queue item already exists" do
+        let!(:queue_item) { Fabricate(:queue_item, package: package, status: :failed) }
+
+        it_behaves_like "creates a new queue item"
+      end
+
+      context "when a queue item does not exist" do
+        it_behaves_like "creates a new queue item"
       end
 
       context "when the policy denies the user access" do
