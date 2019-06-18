cb09baf7e3e55db867600be27666a3bd513686f3
Noah Botimer <botimer@umich.edu>
2019-06-18 19:57:09 -0400

    Add World modules for users/API requests
    This adds a couple of prospective conveniences for writing scenarios,
    specifically around creating an acting user with a particular role and
    making API requests (with the "me" user's API token header).

---
+++ features/support/world_extensions.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require "checkpoint"
+
+class RoleGranter
+  attr_reader :user, :role
+
+  def initialize(user, role, authority: Services.checkpoint)
+    @user = user
+    @role = Checkpoint::Credential::Role.new(role)
+    @authority = authority
+  end
+
+  def on_everything
+    grant! Checkpoint::Resource.all
+  end
+
+  def on_all(type)
+    grant! Checkpoint::Resource::AllOfType.new(type)
+  end
+
+  def on(type, id)
+    grant! Checkpoint::Resource::Token.new(type, id)
+  end
+
+  private
+
+  def grant!(resource)
+    authority.grant!(user, role, resource)
+  end
+
+  attr_reader :authority
+end
+
+module CanManageAccounts
+  def me
+    @me ||= Fabricate(:user, api_key_digest: my_api_key.digest)
+  end
+
+  def make_me_a(role)
+    RoleGranter.new(me, role)
+  end
+
+  def my_api_key
+    @my_api_key ||= Keycard::DigestKey.new
+  end
+
+  alias_method :make_me_an, :make_me_a
+end
+
+module MakesApiRequests
+  def api_get(*args)
+    set_auth_token
+    get(*args)
+  end
+
+  def set_auth_token
+    header("Authorization", "Token token=#{my_api_key}")
+  end
+end
+
+World(CanManageAccounts)
+World(MakesApiRequests)
