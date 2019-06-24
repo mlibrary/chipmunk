ab4ed956d9bc2a7ef28f7ba055fbc583bdac0137
Bryan Hockey <bhock@umich.edu>
2019-06-24 18:16:56 -0500

    Update integration specs
    This addresses integration specs being out of date for two recent changes:
    * fa0f5e3 - Use noop external validators in unit tests
    * d7a591c - IncomingStorage delegates link creation to parameters

--- spec/integration/audio_spec.rb
+++ spec/integration/audio_spec.rb
@@ -7,7 +7,6 @@ RSpec.describe "audio validation integration", integration: true do
   it_behaves_like "a validation integration" do
     let(:content_type) { "audio" }
     let(:external_id) { "39015087086396" }
-    let(:validation_script) { "validate_audio.sh" }
     let(:expected_error) { /ERROR - Missing file.*file: pm000001.wav/m }
   end
 end
