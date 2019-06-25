0a69b44f9ac22a9e82e930a4665a74f20c966681
Bryan Hockey <bhock@umich.edu>
2019-06-25 16:25:44 -0500

    Provision and cleanup test sandbox each cucumber run

--- features/support/sandbox.rb
+++ features/support/sandbox.rb
@@ -1,9 +1,11 @@
 require "fileutils"
 
 Before do
+  FileUtils.mkdir_p Chipmunk.config.upload.storage_path
   FileUtils.mkdir_p Chipmunk.config.upload.upload_path
 end
 
 After do
+  FileUtils.rm_rf Chipmunk.config.upload.storage_path
   FileUtils.rm_rf Chipmunk.config.upload.upload_path
 end
