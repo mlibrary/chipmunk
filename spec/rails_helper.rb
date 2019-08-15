1df073f77ddfd756912628c85addcfc3da5e30a1
Bryan Hockey <bhock@umich.edu>
2019-08-15 13:01:53 -0500

    Move test Services config to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,3 +12,33 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = true
   config.infer_spec_type_from_file_location!
 end
+
+Services.register(:incoming_storage) do
+  Chipmunk::IncomingStorage.new(
+    volume: Chipmunk::Volume.new(
+      name: "incoming",
+      reader: Chipmunk::Bag::Reader.new,
+      writer: Chipmunk::Bag::MoveWriter.new,
+      root_path: Chipmunk.config.upload.upload_path
+    ),
+    paths: Chipmunk::UploadPath.new("/"),
+    links: Chipmunk::UploadPath.new(Chipmunk.config.upload["rsync_point"])
+  )
+end
+
+Services.register(:storage) do
+  Chipmunk::PackageStorage.new(volumes: [
+    Chipmunk::Volume.new(
+      name: "test",
+      reader: Chipmunk::Bag::Reader.new,
+      writer: Chipmunk::Bag::MoveWriter.new,
+      root_path: Rails.root.join("spec/support/fixtures")
+    ),
+    Chipmunk::Volume.new(
+      name: "bags",
+      reader: Chipmunk::Bag::Reader.new,
+      writer: Chipmunk::Bag::MoveWriter.new,
+      root_path: Chipmunk.config.upload.storage_path
+    )
+  ])
+end
