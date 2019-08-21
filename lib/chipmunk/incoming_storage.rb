e716d81bc25c520252f42daa0496ea456f559f39
Bryan Hockey <bhock@umich.edu>
2019-08-21 14:57:48 -0500

    Fix long comment lines in IncomingStorage

--- lib/chipmunk/incoming_storage.rb
+++ lib/chipmunk/incoming_storage.rb
@@ -3,17 +3,19 @@
 require "pathname"
 
 module Chipmunk
-  # IncomingStorage is responsible for the business rules of writing to and reading from
-  # Volumes earmarked for its use. It is specifically concerned with initial upload of bags,
-  # and makes them available to other parts of the system via its methods.
+  # IncomingStorage is responsible for the business rules of writing to and reading
+  # from Volumes earmarked for its use. It is specifically concerned with initial
+  # upload of bags, and makes them available to other parts of the system via its
+  # methods.
   class IncomingStorage
 
     # Create an IncomingStorage instance.
-    # @param volume [Volume] The Volume from which the deposited packages should be ingested
-    # @param paths [PathBuilder] A PathBuilder that returns a path on disk to the user upload
-    #   location for a deposit, for a given package.
-    # @param links [PathBuilder] A PathBuilder that returns an rsync destination to which the
-    #   user should upload, for a given package.
+    # @param volume [Volume] The Volume from which the deposited packages should be
+    #   ingested
+    # @param paths [PathBuilder] A PathBuilder that returns a path on disk to the user
+    #   upload location for a deposit, for a given package.
+    # @param links [PathBuilder] A PathBuilder that returns an rsync destination to
+    #   which the user should upload, for a given package.
     def initialize(volume:, paths:, links:)
       @volume = volume
       @paths = paths
