41898bef4861430e779d7f71c23cb4e7b9dec63d
Bryan Hockey <bhock@umich.edu>
2019-08-29 04:00:34 -0500

    Treat Package, Deposit as depositable

--- lib/chipmunk/incoming_storage.rb
+++ lib/chipmunk/incoming_storage.rb
@@ -10,34 +10,34 @@ module Chipmunk
   class IncomingStorage
 
     # Create an IncomingStorage instance.
-    # @param volume [Volume] The Volume from which the deposited packages should be
-    #   ingested
+    # @param volume [Volume] The Volume from which the deposited depositable items
+    #   should be ingested
     # @param paths [PathBuilder] A PathBuilder that returns a path on disk to the user
-    #   upload location for a deposit, for a given package.
+    #   upload location for a deposit, for a given depositable item.
     # @param links [PathBuilder] A PathBuilder that returns an rsync destination to
-    #   which the user should upload, for a given package.
+    #   which the user should upload, for a given depositable item.
     def initialize(volume:, paths:, links:)
       @volume = volume
       @paths = paths
       @links = links
     end
 
-    def for(package)
-      volume.get(upload_path(package))
+    def for(depositable)
+      volume.get(upload_path(depositable))
     end
 
-    def include?(package)
-      volume.include?(upload_path(package))
+    def include?(depositable)
+      volume.include?(upload_path(depositable))
     end
 
-    def upload_link(package)
-      links.for(package)
+    def upload_link(depositable)
+      links.for(depositable)
     end
 
     private
 
-    def upload_path(package)
-      paths.for(package)
+    def upload_path(depositable)
+      paths.for(depositable)
     end
 
     attr_reader :volume, :paths, :links
